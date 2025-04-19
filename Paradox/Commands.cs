cd9f9200cd7f912290efbe416e166a602238e29e
Kaiser Paul <107715370+callmekaii@users.noreply.github.com>
2025-04-19 19:24:28 +0800

    Update project configuration and implement bot commands
    - Introduced `Commands.cs` for handling bot commands, including a placeholder command.
    - Added JsonParser so we won't be hardcoding the API keys lol

---
+++ Paradox/Commands.cs
@@ -0,0 +1,71 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Paradox.Commands
+{
+    public class Commands : BaseCommandModule
+    {
+        [Command("Hug")]
+        public async Task Hug(CommandContext ctx)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync("https://api.waifu.pics/sfw/hug");
+            var responseString = await response.Content.ReadAsStringAsync();
+            var ImageUrl = JObject.Parse(responseString)["url"].ToString();
+            var embed = new DiscordEmbedBuilder()
+            {
+                Title = "*Hugs You",
+                ImageUrl = ImageUrl,
+                Color = new DiscordColor(0x00ff00)
+            };
+
+            await ctx.RespondAsync(embed);
+        }
+    }
+
+    public class JsonParser
+    {
+        static string json;
+        public static string GoogleAPI;
+        public static string DiscordToken;
+
+        public static void JsonParseChecker()
+        {
+            //checks if path exists
+            if (File.Exists("config.json"))
+            {
+                json = File.ReadAllText("config.json");
+                GoogleAPI = JObject.Parse(json)["GoogleAPI"].ToString();
+                DiscordToken = JObject.Parse(json)["DiscordToken"].ToString();
+
+            }
+
+            else
+            {
+                ConfigCreator creator = new ConfigCreator();
+                Console.WriteLine("Please Enter Discord Token: ");
+                creator.DiscordToken = (Console.ReadLine());
+                Console.WriteLine("Please Enter GoogleAPI: ");
+                creator.GoogleAPI = (Console.ReadLine());
+                var ConfigCreatorSerialized = JsonConvert.SerializeObject(creator);
+                File.WriteAllText("config.json", ConfigCreatorSerialized);
+                JsonParseChecker();
+            }
+        }
+    }
+
+    public class ConfigCreator
+    {
+        public string DiscordToken;
+        public string GoogleAPI;
+        public string UserID;
+        public string Response;
+    }
+}
--- Paradox/Program.cs
+++ Paradox/Program.cs
@@ -1,16 +1,68 @@
-﻿using System;
+﻿using Paradox;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using Paradox.Commands;
 
 namespace Paradox
 {
     class Program
     {
-        static void Main(string[] args)
+        private static DiscordClient Client { get; set; }
+        private static CommandsNextExtension Commands { get; set; }
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Fuck Negers");
+            //This basically is the template of to run the whole ahh bot but putting the token
+            //there unprotected is bad practice
+            JsonParser.JsonParseChecker();
+            var discordConfig = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = Paradox.Commands.JsonParser.DiscordToken,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
+            Client = new DiscordClient(discordConfig);
+            Client.Ready += Client_Ready;
+
+            //This thingy is going to handle the commands
+            var commandsConfig = new CommandsNextConfiguration()
+            {
+                StringPrefixes = new string[] { "hey ", "!", "" },
+                EnableDms = true,
+                EnableMentionPrefix = true,
+                DmHelp = true
+            };
+
+            //Prefix Commands Registration
+            Commands = Client.UseCommandsNext(commandsConfig);
+            Commands.RegisterCommands<Commands.Commands>();
+
+
+            //Connects to the server
+            try
+            {
+                await Client.ConnectAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Token is invalid. Input the token again");
+                File.Delete("config.json");
+                JsonParser.JsonParseChecker();
+            }
+
+            await Task.Delay(-1);
+        }
+
+        private static Task Client_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
         }
     }
 }
