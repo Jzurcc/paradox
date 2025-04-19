583ab36a405106579743e9aee69e9e9ccb4609a4
John Reniel Cruz <johnreniel042206@gmail.com>
2025-04-20 02:00:26 +0800

    Added sample commands, Gemini integration, and chat sessions

--- Paradox/Commands.cs
+++ Paradox/Commands.cs
@@ -1,9 +1,12 @@
 ﻿using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
+using GenerativeAI;
+using GenerativeAI.Types;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -12,22 +15,65 @@ namespace Paradox.Commands
 {
     public class Commands : BaseCommandModule
     {
+        // preload GoogleAI
+        static GoogleAi googleAI = new GoogleAi(JsonParser.GeminiAPI);
+        static GenerativeModel model = googleAI.CreateGenerativeModel("gemini-2.0-flash-lite");
+        //var history = new List<Content>
+        //    {
+        //        new Content("from now on, you're my slave",  Roles.User),
+        //    };
+        
+        List<Content> history = 
+        [
+            new Content("You are a multiverse RPG system.", Roles.User), 
+            new Content(model.GenerateContentAsync("Create an absurdly specific multiverse with 3 tiers of items, enemies, and NPCs.").ToString(), Roles.Model)        
+        ];
+        ChatSession chatSession = model.StartChat();
+
+        [Command("Generate")]
+        public async Task Generate(CommandContext ctx, [RemainingText] string input)
+        {
+            //var response = await chatSession.GenerateContentAsync(input);
+            var response = await chatSession.GenerateContentAsync(input);
+            await ctx.RespondAsync(response.Text());
+        }
+
         [Command("Hug")]
         public async Task Hug(CommandContext ctx)
         {
             HttpClient client = new HttpClient();
-            var response = await client.GetAsync("https://api.waifu.pics/sfw/hug");
-            var responseString = await response.Content.ReadAsStringAsync();
-            var ImageUrl = JObject.Parse(responseString)["url"].ToString();
+            var media = await client.GetAsync("https://api.waifu.pics/sfw/hug");
+            var mediaString = await media.Content.ReadAsStringAsync();
+            var mediaUrl = JObject.Parse(mediaString)["url"].ToString();
             var embed = new DiscordEmbedBuilder()
             {
                 Title = "*Hugs You",
-                ImageUrl = ImageUrl,
+                ImageUrl = mediaUrl,
                 Color = new DiscordColor(0x00ff00)
             };
 
             await ctx.RespondAsync(embed);
         }
+        [Command("Start")]
+        public async Task Start(CommandContext ctx)
+        {
+            var response = await chatSession.GenerateContentAsync("Welcome the player and act like a title menu");
+            await ctx.RespondAsync(response.Text());
+        }
+        [Command("Inventory")]
+        public async Task Inventory(CommandContext ctx)
+        {
+            var response = await chatSession.GenerateContentAsync("Create a well-designed text inventory system filled with random loot (multiverse style)");
+            await ctx.RespondAsync(response.Text());
+        }
+
+        [Command("Act")]
+        public async Task Act(CommandContext ctx)
+        { 
+            var response = await chatSession.GenerateContentAsync("Only accept reasonable actions within the game. The success of an action is dependent on ratio on dice rolls that can either be definite, d2, d6, d12, etc. depending on complexity. If action doesn't fall under this category, ask them again. Tell some optional choices (but other choices can be made) and a description of the choice and context. User Message: " + ctx.Message);
+            await ctx.RespondAsync(response.Text());
+        }
+
     }
 
     public class JsonParser
